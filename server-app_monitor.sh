f8674b328a81143acf7e13c73fd4377b7cf1aec6
Gianluca Lodo <gianluca.lodo@gmail.com>
2017-11-06 19:49:54 +0100

    Create server-app_monitor.sh

---
+++ server-app_monitor.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/ksh
+
+#this script launch WLST server-app_monitor.pj
+
+WORKDIR="<set here workdir>"
+
+
+BANNER="WARNING... Weblogic connection problem !"
+
+clear
+
+print "\nWLS server and app check\n"
+
+. ${WORKDIR}/setWLSTEnv.sh
+${JAVA_OPT} -cp ${CLASSPATH} weblogic.WLST ${WORKDIR}/server-app_monitor -p ${WORKDIR} 2>/dev/null
+
+#return 0 -> OK
+#return different from 0 -> NOK 
+if [ $? -ne 0 ]
+then
+        banner "ATTENZIONEr!"
+        print "\n\n\n${BANNER}\n\n"
+        print "ERRORS:\n\n${BANNER}"
+
+fi
+
+print "\n\n"
